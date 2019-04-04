957afd22c7a8afec6d5364174cf133989e8e63e7
SerhiiNovitskyi <sergi.novitskiy@gmail.com>
2019-04-04 11:58:52 +0300

    first commit

---
+++ src/main/java/SecondLesson.java
@@ -0,0 +1,15 @@
+public class SecondLesson {
+    public static void main(String[] args) {
+        String name ="Vasyl";
+        int age = 17;
+        double score = 14.7;
+        char group = 'A';
+        boolean online = true;
+
+
+        System.out.println(score + age);
+        System.out.println(group + name);
+        System.out.println(age + name);
+        System.out.println(name + online);
+    }
+}
